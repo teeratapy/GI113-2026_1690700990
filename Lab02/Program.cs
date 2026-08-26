bc6f6d8feb1fd0420c1a08c96a04e5d2743841b2
teeratap <teeratap.yote@bumail.net>
2026-08-26 13:06:52 +0700

    Add greeting message to console output

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -6,12 +6,15 @@
  * Course     : GI113 Computer Programming (GI)
  */
 
+using System;
+
 namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
             string bossName = "Kirin";
             char rank = 'S';
             int level = 7;
